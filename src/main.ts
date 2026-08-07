d773c97344c80fdf3532eace9a98b32d3fd44202
Alejandro Forero Cuervo <alefore@gmail.com>
2026-08-07 22:24:45 +0200

    Start creating a class to hold the application state.

--- src/main.ts
+++ src/main.ts
@@ -12,44 +12,6 @@ class Unit {
   constructor(public readonly name: string) {}
 }
 
-const data = new Map<Unit, UnitEntry[]>();
-
-const yearUnit = new Unit('Year');
-
-data.set(
-    yearUnit,
-    historicalEvents
-        .concat(famousBirths.map(
-            (b: FamousBirth) =>
-                ({name: `👶 ${b.name} was born`, value: b.value, id: b.name})))
-        .concat(
-            books.map((b: Book) => ({
-                        name: `📖 ${b.title} (by ${b.author}) was published`,
-                        value: b.year,
-                        id: b.author
-                      })))
-        .concat(inventions.map((i: Invention) => ({
-                                 name: `💡 ${i.name} was invented` +
-                                     (i.inventor ? ` (by ${i.inventor})` : ''),
-                                 value: i.year,
-                                 id: i.inventor
-                               })))
-        .concat(paintings.map((p: Painting) => ({
-                                name: `🎨 ${p.artist} finished ${p.title}`,
-                                value: p.year,
-                                id: p.artist
-                              })))
-        .concat(structures.map(
-            (s: Structure) => ({
-              name: `🏛️  ${s.name} (${s.country}) was built (finished)`,
-              value: s.year
-            })))
-        .concat(films.map((f: Film) => ({
-                            name: `🎥 ${f.title} (${f.director}) was released`,
-                            value: f.year
-                          })))
-        .sort((a, b) => a.value - b.value));
-
 function randomGaussian(mean: number, stdDev: number): number {
   const u = 1 - Math.random();  // Prevents Math.log(0)
   const v = Math.random();
@@ -57,90 +19,172 @@ function randomGaussian(mean: number, stdDev: number): number {
       Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * stdDev;
 }
 
-function generateQuestion(data: Map<Unit, UnitEntry[]>): CompareQuestion {
-  const units = Array.from(data.keys());
-  if (units.length === 0) throw new Error('Data map is empty.');
+class App {
+  readonly data = new Map<Unit, UnitEntry[]>();
+  readonly historyDiv =
+      Object.assign(document.createElement('div'), {id: 'history'});
+  readonly gameDiv = document.createElement('div');
 
-  const entries = data.get(units[Math.floor(Math.random() * units.length)])!;
-  if (entries.length < 2) throw new Error('Not enough entries to compare.');
+  constructor() {
+    const yearUnit = new Unit('Year');
+    this.data.set(
+        yearUnit,
+        historicalEvents
+            .concat(famousBirths.map((b: FamousBirth) => ({
+                                       name: `👶 ${b.name} was born`,
+                                       value: b.value,
+                                       id: b.name
+                                     })))
+            .concat(
+                books.map((b: Book) => ({
+                            name: `${b.title} (by ${b.author}) was published`,
+                            value: b.year
+                          })))
+            .concat(
+                inventions.map((i: Invention) => ({
+                                 name: `${i.name} was invented` +
+                                     (i.inventor ? ` (by ${i.inventor})` : ''),
+                                 value: i.year,
+                                 id: i.inventor
+                               })))
+            .concat(paintings.map((p: Painting) => ({
+                                    name: `🎨 ${p.artist} finished ${p.title}`,
+                                    value: p.year,
+                                    id: p.artist
+                                  })))
+            .concat(structures.map(
+                (s: Structure) => ({
+                  name: `🏛️  ${s.name} (${s.country}) was built (finished)`,
+                  value: s.year
+                })))
+            .concat(
+                films.map((f: Film) => ({
+                            name: `🎥 ${f.title} (${f.director}) was released`,
+                            value: f.year
+                          })))
+            .sort((a, b) => a.value - b.value));
 
-  const base = entries[Math.floor(Math.random() * entries.length)];
+    this.gameDiv.append(this.startGameButton());
 
-  const minYear = entries[0].value;
-  const maxYear = entries[entries.length - 1].value;
-  if (minYear === maxYear) {
-    throw new Error(
-        'All entries occur in the exact same year. Cannot generate a comparison.');
+    document.body.append(this.historyDiv, this.gameDiv);
+
+    const records: GameRecord[] = loadHistory();
+    if (records.length > 0) displayHistory(records);
   }
 
-  const currentYear = new Date().getFullYear();
-  // 20% of elapsed time or at least 5 years.
-  const targetDistance = Math.max(5, (currentYear - base.value) * 0.20);
+  generateQuestion(): CompareQuestion {
+    const units = Array.from(this.data.keys());
+    if (units.length === 0) throw new Error('Data map is empty.');
 
-  let targetYear: number;
-  do {
-    targetYear = randomGaussian(base.value, targetDistance);
-  } while (targetYear < minYear || targetYear > maxYear);
+    const entries =
+        this.data.get(units[Math.floor(Math.random() * units.length)])!;
+    if (entries.length < 2) throw new Error('Not enough entries to compare.');
 
-  const nearest10 =
-      entries.filter(entry => entry.value !== base.value)
-          .filter(entry => !base.id || !entry.id || base.id !== entry.id)
-          .sort(
-              (a, b) => Math.abs(a.value - targetYear) -
-                  Math.abs(b.value - targetYear))
-          .slice(0, 10);
+    const base = entries[Math.floor(Math.random() * entries.length)];
 
-  if (nearest10.length === 0) {
-    throw new Error('Could not find a second entry with a different value.');
+    const minYear = entries[0].value;
+    const maxYear = entries[entries.length - 1].value;
+    if (minYear === maxYear) {
+      throw new Error(
+          'All entries occur in the exact same year. Cannot generate a comparison.');
+    }
+
+    const currentYear = new Date().getFullYear();
+    // 20% of elapsed time or at least 5 years.
+    const targetDistance = Math.max(5, (currentYear - base.value) * 0.20);
+
+    let targetYear: number;
+    do {
+      targetYear = randomGaussian(base.value, targetDistance);
+    } while (targetYear < minYear || targetYear > maxYear);
+
+    const nearest10 =
+        entries.filter(entry => entry.value !== base.value)
+            .filter(entry => !base.id || !entry.id || base.id !== entry.id)
+            .sort(
+                (a, b) => Math.abs(a.value - targetYear) -
+                    Math.abs(b.value - targetYear))
+            .slice(0, 10);
+
+    if (nearest10.length === 0) {
+      throw new Error('Could not find a second entry with a different value.');
+    }
+
+    return new CompareQuestion(
+        base, nearest10[Math.floor(Math.random() * nearest10.length)]);
   }
 
-  return new CompareQuestion(
-      base, nearest10[Math.floor(Math.random() * nearest10.length)]);
+  startGameButton() {
+    const button = document.createElement('button');
+    button.textContent = 'Start new game';
+    button.addEventListener('click', (event: MouseEvent) => this.startGame());
+    return button;
+  }
+
+  gameDone(questions: QuestionView[]) {
+    this.gameDiv.append(this.startGameButton());
+    questions.forEach((q) => q.reveal());
+
+    const records: GameRecord[] = saveGame(
+        questions.map((q) => Number(q.slider.value) / 100),
+        questions.filter((q) => q.question.isCorrect()).length);
+
+    displayHistory(records);
+  }
+
+  startGame() {
+    this.gameDiv.replaceChildren();
+    const questionInputs =
+        Array.from({length: 10}, () => this.generateQuestion());
+    const questions: QuestionView[] = questionInputs.map((q) => {
+      return new QuestionView(q, this.gameDiv);
+    });
+
+    const button = document.createElement('button');
+    button.textContent = 'Finish';
+    button.addEventListener('click', (event: MouseEvent) => {
+      this.gameDone(questions);
+      button.remove();
+    });
+    this.gameDiv.append(button);
+  }
 }
 
-function startGameButton(gameDiv: HTMLDivElement) {
-  const button = document.createElement('button');
-  button.textContent = 'Start new game';
-  button.addEventListener('click', (event: MouseEvent) => startGame(gameDiv));
-  return button;
-}
-
-function gameDone(questions: QuestionView[], gameDiv: HTMLDivElement) {
-  gameDiv.append(startGameButton(gameDiv));
-  questions.forEach((q) => q.reveal());
-
-  const records: GameRecord[] = saveGame(
-      questions.map((q) => Number(q.slider.value) / 100),
-      questions.filter((q) => q.question.isCorrect()).length);
-
-  displayHistory(records);
-}
-
-function startGame(gameDiv: HTMLDivElement) {
-  gameDiv.replaceChildren();
-  const questionInputs = Array.from({length: 10}, () => generateQuestion(data));
-  const questions: QuestionView[] = questionInputs.map((q) => {
-    return new QuestionView(q, gameDiv);
-  });
-
-  const button = document.createElement('button');
-  button.textContent = 'Finish';
-  button.addEventListener('click', (event: MouseEvent) => {
-    gameDone(questions, gameDiv);
-    button.remove();
-  });
-  gameDiv.append(button);
-}
+// const xquestionInputs: QuestionInput[] = [
+//   {question: 'Alejo was born in Argentina', correctAnswer: false},
+//   {
+//     question:
+//         'Brazil is more than 100 times bigger (land area) than Switzerland',
+//     correctAnswer: true
+//   },
+//   {
+//     question:
+//         '20 <= number of Swiss cantons <= 30 (counting half-cantons as 0.5)',
+//     correctAnswer: true
+//   },
+//   {
+//     question: 'Switzerland has borders with exactly 5 countries',
+//     correctAnswer: true
+//   },
+//   {
+//     question: 'Less than 15% of the Alps (land area) is in Switzerland',
+//     correctAnswer: true
+//   },
+//   {
+//     question: 'The world\'s first website had a ".ch" domain name.',
+//     correctAnswer: true
+//   },
+//   {question: '1900 <= Nintendo was founded < 2000', correctAnswer: false},
+//   {
+//     question: 'The term "robot" was coined by a Polish writer',
+//     correctAnswer: false
+//   },
+//   {
+//     question: 'Smartphones with built-in GPS were released in 1996',
+//     correctAnswer: false
+//   },
+// ];
 
 document.addEventListener('DOMContentLoaded', () => {
-  const historyDiv =
-      Object.assign(document.createElement('div'), {id: 'history'});
-
-  const gameDiv = document.createElement('div');
-  gameDiv.append(startGameButton(gameDiv));
-
-  document.body.append(historyDiv, gameDiv);
-
-  const records: GameRecord[] = loadHistory();
-  if (records.length > 0) displayHistory(records);
+  new App();
 });
